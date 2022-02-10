b624facf190c368907f50e544ce0de6dc4175ae3
scorpion712 <d.lautaro712@gmail.com>
2022-02-09 23:30:35 -0300

    Add search bar

--- frontend/components/FoodTable.tsx
+++ frontend/components/FoodTable.tsx
@@ -4,12 +4,19 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import EnhancedTable from './Table/EnhancedTable';
-import {menues} from '../utils/table/FoodTable';
-import {headCells} from '../utils/table/FoodTable';
+import { menues } from '../utils/table/FoodTable';
+import { headCells } from '../utils/table/FoodTable';
+import { Menu } from '../utils/interfaces';
 
-export default function FoodTable() {
+interface FoodTableProps {
+  search: string,
+};
 
-  const buttons = [ 
+export default function FoodTable(props: FoodTableProps) {
+
+  const { search } = props;
+
+  const buttons = [
     <IconButton key="b1" onClick={() => console.log('clic')}>
       <EditIcon />
     </IconButton>,
@@ -17,7 +24,8 @@ export default function FoodTable() {
       <DeleteIcon />
     </IconButton>
   ];
-  
 
-  return <EnhancedTable rows={menues} headCells={headCells} buttons={buttons} />;
+  const foodRows = menues.filter(({ name }: Menu) => name.toLowerCase().includes(search));
+
+  return <EnhancedTable rows={foodRows} headCells={headCells} buttons={buttons} />;
 }
--- frontend/pages/FoodPage.tsx
+++ frontend/pages/FoodPage.tsx
@@ -1,23 +1,28 @@
-import { Autocomplete, TextField } from '@mui/material';
+import { TextField } from '@mui/material';
 import React from 'react';
 import FoodTable from '../components/FoodTable';
 
 import Layout from '../components/Layout';
 
 export default function FoodPage(): JSX.Element {
+
+  const [search, setSearch] = React.useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setSearch(e.target.value.toLowerCase());
+  }
+
   return (
     <Layout>
       <div>
-        <Autocomplete
+        <TextField
           style={{ margin: '1rem 0 .5rem 0' }}
-          id="food-search"
-          freeSolo
-          options={[]}
-          renderInput={(params) => (
-            <TextField {...params} label="Buscar menu" />
-          )}
+          fullWidth
+          placeholder='Buscar Menu...'
+          value={search}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => handleChange(e)}
         />
-        <FoodTable />
+        <FoodTable search={search}/>
       </div>
     </Layout>
   );
