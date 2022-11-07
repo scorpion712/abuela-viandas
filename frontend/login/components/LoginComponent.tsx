aa2846b64efbf312386814da288644bf266a614c
scorpion712 <lauutii.127@gmail.com>
2022-11-06 23:50:37 -0300

    Refactoring component and creating login services template

---
+++ frontend/login/components/LoginComponent.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import { Link, Typography } from "@mui/material";
+import {
+  authFacebook,
+  authFirebase,
+  authGoogle,
+} from "../services/login.service";
+
+export const LoginComponent = () => {
+  const [authWay, setAuthWay] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log(authWay)
+    switch (authWay) {
+      case "facebook":
+        authFacebook(dispatch, data);
+        break;
+      case "google":
+        authGoogle(dispatch, data);
+        break;
+      default:
+        authFirebase(dispatch, data);
+        break;
+    }
+  };
+
+  const handleLogin = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setAuthWay(event.currentTarget.name);
+  };
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Iniciar Sesión
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Dirección de correo electrónico"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Contraseña"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Recordarme"
+        />
+        <Button
+          type="submit"
+          name="firebase"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3 }}
+          onClick={(e) => handleLogin(e)}
+        >
+          Iniciar Sesión
+        </Button>
+        <Button
+          type="submit"
+          name="google"
+          fullWidth
+          variant="outlined"
+          sx={{ mt: 2, mb: 2 }}
+          style={{ backgroundColor: "#DB4437", color: "#FFF" }}
+          startIcon={<GoogleIcon style={{ color: "#FFF" }} />}
+          onClick={(e) => handleLogin(e)}
+        >
+          Iniciar Sesión con Google
+        </Button>
+        <Button
+          type="submit"
+          name="facebook"
+          fullWidth
+          variant="contained"
+          sx={{ mb: 2 }}
+          style={{ backgroundColor: "#3b5998" }}
+          startIcon={<FacebookIcon />}
+          onClick={(e) => handleLogin(e)}
+        >
+          Iniciar Sesión con Facebook
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link href="#" variant="body2">
+              Olvidó su contraseña?
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="signup" variant="body2">
+              {"Aún no tiene cuenta? Registrese"}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import GoogleIcon from '@mui/icons-material/Google';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import Layout from "../components/Layout";
+import { LoginComponent } from "../login/components/LoginComponent";
 
 function Copyright(props: any) {
   return (
@@ -33,98 +25,13 @@ function Copyright(props: any) {
 }
 
 export default function Login() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
-
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Iniciar Sesión
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Dirección de correo electrónico"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Contraseña"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Recordarme"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Iniciar Sesión 
-          </Button>
-          <Button
-            type="submit"
-            fullWidth
-            variant="outlined"
-            sx={{ mt: 3, mb: 2,  }}
-            startIcon={<GoogleIcon color='error'/>}
-          >
-            Iniciar Sesión  con Google
-          </Button>
-          <Button
-            type="submit"
-            fullWidth
-            variant="outlined"
-            sx={{ mt: 3, mb: 2 }}
-            startIcon={<FacebookIcon color='info'/>}
-          >
-            Iniciar Sesión con Facebook
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Olvidó su contraseña?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Aún no tiene cuenta? Registrese"}
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
-    </Container>
+    <Layout>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline /> 
+        <LoginComponent />
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </Layout>
   );
 }
